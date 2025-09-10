8fdd7fad281cdebe258fd453f9a0342ace51bb21
Q-VD <qvandommelen55@gmail.com>
2025-09-10 18:44:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function begroet() {
   let naam = document.getElementById("naamInput").value;
     document.getElementById("groet").textContent = "Hallo " + naam + "!";
 }
+
+function bereken() {
+  let a = parseInt(document.getElementById("a").value);
+  let b = parseInt(document.getElementById("b").value);
+  document.getElementById("resultaat").textContent = "Resultaat: " (a + b);
+}
