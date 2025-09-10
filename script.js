db466ca4f5427adb56bb3a97980aa8de9a56fd5a
Q-VD <qvandommelen55@gmail.com>
2025-09-10 18:18:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
 document.getElementById("berichtje").textContent = "Hallo wereld vanuit Javascript!";
-fucntion groet() {
-  document.getElementById("uitkomst").textContent = "Je hebt geklikt!";
-}
+
