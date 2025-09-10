a83969a44e12ada6ab5f3df52bab4c4818134ad7
Q-VD <qvandommelen55@gmail.com>
2025-09-10 18:54:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.getElementById("berichtje").textContent = "Hallo wereld vanuit Javascript!";
-
+// begroeting
 function begroet() {
   let naam = document.getElementById("naamInput").value;
     document.getElementById("groet").textContent = "Hallo " + naam + "!";
 }
-
+// mini rekenmachine
 function bereken() {
   let a = parseInt(document.getElementById("a").value);
   let b = parseInt(document.getElementById("b").value);
   document.getElementById("resultaat").textContent = "Resultaat: " + (a + b);
+}
+//todo list
+let taken = [];
+
+function voegtoe() {
+  let taak = document.getElementById("taak").value;
+  taken.push(taak);
+  document.getElementById("lijst").innerHTML = taken.map(t => "<li>" + t + "<li>").join("");
 }
